fbb7cb81eb00473bce06f77a4735a8f440743145
Anubhav soni <112082648+Anubhavsoni001@users.noreply.github.com>
2024-12-27 16:33:05 +0530

    Add files via upload

---
+++ src/main/java/com/backend/ai/BackendAiApplication.java
@@ -0,0 +1,17 @@
+package com.backend.ai;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAiApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {SpringApplication.run(BackendAiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
